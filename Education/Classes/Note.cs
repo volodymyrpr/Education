e056a782d5fc53c8e4b20c3f77e9aa8fbd003776
volodymyrpr <volodymyrprfromukraine@gmail.com>
2016-12-09 01:32:23 +0200

    IComparable, IEquatable

---
+++ Education/Classes/Note.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Classes
+{
+    public struct Note : IComparable<Note>, IEquatable<Note>, IComparable
+    {
+        private int semitonesFromA;
+        public int SemitonesFromA { get { return semitonesFromA; } }
+
+        public Note(int semitonesFromA)
+        {
+            this.semitonesFromA = semitonesFromA;
+        }
+
+        public int CompareTo(object other)
+        {
+            if (!(other is Note))
+            {
+                throw new InvalidOperationException("Compare to: not a note");
+            }
+
+            return CompareTo((Note)other);
+        }
+
+        public int CompareTo(Note other)
+        {
+            if (Equals(other))
+            {
+                return 0;
+            }
+
+            return semitonesFromA.CompareTo(other.semitonesFromA);
+        }
+
+        public static bool operator < (Note n1, Note n2)
+        {
+            return n1.CompareTo(n2) < 0;
+        }
+
+        public static bool operator > (Note n1, Note n2)
+        {
+            return n1.CompareTo(n2) > 0;
+        }
+
+        public bool Equals(Note other) => semitonesFromA == other.semitonesFromA;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Note))
+            {
+                return false;
+            }
+
+            return Equals((Note)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return semitonesFromA.GetHashCode();
+        }
+
+        public static bool operator == (Note n1, Note n2) => n1.Equals(n2);
+
+        public static bool operator != (Note n1, Note n2) => !(n1 == n2);
+    }
+}
