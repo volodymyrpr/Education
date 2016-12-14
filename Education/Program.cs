cd3937f5ad1c583b11e90706c17af6696b1c0c95
volodymyrpr <volodymyrprfromukraine@gmail.com>
2016-12-14 22:31:37 +0200

    Console

--- Education/Program.cs
+++ Education/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Drawing;
 using System.Threading;
 using System.Numerics;
+using System.Diagnostics;
 
 namespace Education
 {
@@ -22,26 +23,17 @@ namespace Education
 
         private void DoEverything()
         {
-            TextWriter oldOut = Console.Out;
-
-            using (TextWriter w = File.CreateText("e:\\output.txt"))
+            ProcessStartInfo psi = new ProcessStartInfo
             {
-                Console.SetOut(w);
+                FileName = "cmd.exe",
+                Arguments = "/c ipconfig /all",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
 
-                Console.WriteLine("test... 30%");
-
-                Thread.Sleep(1500);
-
-                Console.Write("test... 50%");
-
-                Thread.Sleep(1500);
-
-                Console.Write("test... 80%");
-            }
-
-            Console.SetOut(oldOut);
-
-            System.Diagnostics.Process.Start("e:\\output.txt");
+            Process p = Process.Start(psi);
+            string result = p.StandardOutput.ReadToEnd();
+            Console.WriteLine(result);
         }
     }
 }
