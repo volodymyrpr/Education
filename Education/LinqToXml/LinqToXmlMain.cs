cd7b02c075ddb2200fba9cd807cef2b4f139ef20
volodymyrpr <volodymyrprfromukraine@gmail.com>
2017-01-26 08:44:12 +0200

    LinqToXml initial

---
+++ Education/LinqToXml/LinqToXmlMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Education.LinqToXml
+{
+    class LinqToXmlMain : IExecutable
+    {
+        public void Execute()
+        {
+            
+        }
+
+        private void XDomOverviewExample()
+        {
+            string xml = @"<customer id='123' status='archived'>
+                            <firstname>Joe</firstname>
+                            <lastname>Bloggs<!--nice name--></lastname>
+                        </customer>";
+
+            XElement customer = XElement.Parse(xml);
+        }
+    }
+}
